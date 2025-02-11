49a390317ae486ba9295c6cc6d8bee0d07e39bbb
rutchand <rutchand@LIN-5CG134BQ37>
2025-02-11 16:55:59 +0530

    Add project files.

---
+++ githubpractice1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace githubpractice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
