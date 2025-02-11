aae8e33004245e156132c5de6fcbc5802698085f
rutchand <rutchand@LIN-5CG134BQ37>
2025-02-11 16:58:07 +0530

    demo 1 added

---
+++ githubpractice1/Demo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubpractice1
+{
+    internal class Demo1
+    {
+        public static void Drive()
+        {
+            Console.WriteLine("driving....");
+        }
+    }
+}
